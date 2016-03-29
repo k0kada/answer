9bec429a229e479e93999d4773cdea1f4d993f3e
Koh Okada <koh.okada@gmail.com>
2016-03-29 12:50:34 +0900

    長テーブルのうなぎ屋

--- test.php
+++ test.php
@@ -1,17 +1,73 @@
-<!--http://paiza.jp/challenges/practice-->
+<!--https://paiza.jp/learning/word-count-->
 
 
 <?php
-$line_size = fgets(STDIN, 256);
- 
-for( $i = 0; $i < $line_size ; $i++){
-    $year = fgets(STDIN, 256);
-    $year = rtrim($year);
-    //うるう年判定
-    if (($year % 4 == 0 && $year % 100 != 0) || $year % 400 == 0) {
-          echo $year." is a leap year\n";
-    } else {
-          echo $year." is not a leap year\n";
+  $result = array();
+  $n = 0;  //席の数
+  $m = 0;  //グループ数
+
+  while(true) {
+    $input = trim(fgets(STDIN));
+    $input_array = explode(" ", $input);
+    
+    if ($n === 0) {  //最初の入力の場合
+      $n = $input_array[0];
+      $m = $input_array[1];
+      //要素数を席の数にする
+      $result = array_fill(0, $input_array[0], null);
+      
+    } elseif ($input !== '') {  //何かしら入力されていて、２回目以降の入力だったら
+       if (checkSeat($result, $n, $input_array[0], $input_array[1])) {
+         $result = sit($result, $n, $input_array[0], $input_array[1]);
+       }
     }
-}
+
+    //入力が終了したら
+    if ($input === '') {
+      $sit_num = 0; //座っている人数
+      foreach ($result as $atom) {
+        $sit_num += $atom;
+      }
+      echo $sit_num. "\n";
+        break;
+    }
+  }
+
+  /**
+   * 座れるかを判定
+   * @param type $result
+   * @param type $n 席の数
+   * @param type $a 来店人数
+   * @param type $b 座る席のスタート場所
+   * @return boolean
+   */
+  function checkSeat($result, $n, $a, $b) {
+    for ($i = $b - 1; $i < $a + ($b - 1); $i++) {
+      $index = $i % $n;
+
+      if ($result[$index] === 1) {
+        return false;
+            
+      }
+    }
+    return true;
+  }
+
+  /**
+   * 人を座らせる
+   * @param int $result
+   * @param type $n 席の数
+   * @param type $a 来店人数
+   * @param type $b 座る席のスタート場所
+   * @return int
+   */
+  function sit($result, $n, $a, $b) {
+    
+    for ($i = $b - 1; $i < $a + ($b - 1); $i++) {
+      $index = $i % $n;
+      $result[$index] = 1;
+    }
+    return $result;
+  }
+  
 ?>
